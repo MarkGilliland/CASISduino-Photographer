6b192fda8339ca674f43a6b11aa9bf993aff50b1
Mark Gilliland <markg773@gmail.com>
2016-08-13 20:39:05 -0600

    Initial Version, takes pictures with no interval

---
+++ CASISduino Photographer/CASISduino Photographer/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace CASISduino_Photographer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBoxVideoDisplay = new System.Windows.Forms.PictureBox();
+            this.comboBoxCamSelect = new System.Windows.Forms.ComboBox();
+            this.buttonOpenCamera = new System.Windows.Forms.Button();
+            this.labelRecentFilename = new System.Windows.Forms.Label();
+            this.buttonSavePicture = new System.Windows.Forms.Button();
+            this.buttonChageDir = new System.Windows.Forms.Button();
+            this.directoryDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.labelDirectory = new System.Windows.Forms.Label();
+            this.labelFilePrefix = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBoxInterval = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBoxCameraProperties = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBoxFileProperties = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxVideoDisplay)).BeginInit();
+            this.groupBoxCameraProperties.SuspendLayout();
+            this.groupBoxFileProperties.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBoxVideoDisplay
+            // 
+            this.pictureBoxVideoDisplay.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.pictureBoxVideoDisplay.Location = new System.Drawing.Point(12, 12);
+            this.pictureBoxVideoDisplay.Name = "pictureBoxVideoDisplay";
+            this.pictureBoxVideoDisplay.Size = new System.Drawing.Size(640, 360);
+            this.pictureBoxVideoDisplay.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxVideoDisplay.TabIndex = 1;
+            this.pictureBoxVideoDisplay.TabStop = false;
+            this.pictureBoxVideoDisplay.Click += new System.EventHandler(this.pictureBoxVideoDisplay_Click);
+            // 
+            // comboBoxCamSelect
+            // 
+            this.comboBoxCamSelect.FormattingEnabled = true;
+            this.comboBoxCamSelect.Location = new System.Drawing.Point(133, 20);
+            this.comboBoxCamSelect.Name = "comboBoxCamSelect";
+            this.comboBoxCamSelect.Size = new System.Drawing.Size(180, 21);
+            this.comboBoxCamSelect.TabIndex = 2;
+            // 
+            // buttonOpenCamera
+            // 
+            this.buttonOpenCamera.Location = new System.Drawing.Point(6, 19);
+            this.buttonOpenCamera.Name = "buttonOpenCamera";
+            this.buttonOpenCamera.Size = new System.Drawing.Size(113, 23);
+            this.buttonOpenCamera.TabIndex = 3;
+            this.buttonOpenCamera.Text = "Open Camera";
+            this.buttonOpenCamera.UseVisualStyleBackColor = true;
+            this.buttonOpenCamera.Click += new System.EventHandler(this.buttonTakePicture_Click);
+            // 
+            // labelRecentFilename
+            // 
+            this.labelRecentFilename.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelRecentFilename.Location = new System.Drawing.Point(4, 98);
+            this.labelRecentFilename.Name = "labelRecentFilename";
+            this.labelRecentFilename.Size = new System.Drawing.Size(313, 20);
+            this.labelRecentFilename.TabIndex = 4;
+            this.labelRecentFilename.Text = "Most Recent Filename:";
+            this.labelRecentFilename.Click += new System.EventHandler(this.labelTimestamp_Click);
+            // 
+            // buttonSavePicture
+            // 
+            this.buttonSavePicture.Location = new System.Drawing.Point(161, 42);
+            this.buttonSavePicture.Name = "buttonSavePicture";
+            this.buttonSavePicture.Size = new System.Drawing.Size(155, 23);
+            this.buttonSavePicture.TabIndex = 5;
+            this.buttonSavePicture.Text = "Save Picture";
+            this.buttonSavePicture.UseVisualStyleBackColor = true;
+            this.buttonSavePicture.Click += new System.EventHandler(this.buttonSavePicture_Click);
+            // 
+            // buttonChageDir
+            // 
+            this.buttonChageDir.Location = new System.Drawing.Point(3, 42);
+            this.buttonChageDir.Name = "buttonChageDir";
+            this.buttonChageDir.Size = new System.Drawing.Size(158, 23);
+            this.buttonChageDir.TabIndex = 6;
+            this.buttonChageDir.Text = "Change Directory";
+            this.buttonChageDir.UseVisualStyleBackColor = true;
+            this.buttonChageDir.Click += new System.EventHandler(this.buttonChageDir_Click);
+            // 
+            // labelDirectory
+            // 
+            this.labelDirectory.Location = new System.Drawing.Point(3, 68);
+            this.labelDirectory.Name = "labelDirectory";
+            this.labelDirectory.Size = new System.Drawing.Size(312, 30);
+            this.labelDirectory.TabIndex = 7;
+            this.labelDirectory.Text = "No Directory Selected";
+            this.labelDirectory.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // labelFilePrefix
+            // 
+            this.labelFilePrefix.Location = new System.Drawing.Point(120, 16);
+            this.labelFilePrefix.Name = "labelFilePrefix";
+            this.labelFilePrefix.Size = new System.Drawing.Size(194, 20);
+            this.labelFilePrefix.TabIndex = 8;
+            this.labelFilePrefix.Text = "Image";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(3, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 17);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "File Prefix: ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // comboBoxInterval
+            // 
+            this.comboBoxInterval.FormattingEnabled = true;
+            this.comboBoxInterval.Location = new System.Drawing.Point(161, 118);
+            this.comboBoxInterval.Name = "comboBoxInterval";
+            this.comboBoxInterval.Size = new System.Drawing.Size(155, 21);
+            this.comboBoxInterval.TabIndex = 10;
+            this.comboBoxInterval.SelectedIndexChanged += new System.EventHandler(this.comboBoxInterval_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(3, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 28);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Interval Between Pictures\r\n(Custom Allowed)";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // groupBoxCameraProperties
+            // 
+            this.groupBoxCameraProperties.Controls.Add(this.label3);
+            this.groupBoxCameraProperties.Controls.Add(this.buttonOpenCamera);
+            this.groupBoxCameraProperties.Controls.Add(this.comboBox1);
+            this.groupBoxCameraProperties.Controls.Add(this.comboBoxCamSelect);
+            this.groupBoxCameraProperties.Location = new System.Drawing.Point(664, 12);
+            this.groupBoxCameraProperties.Name = "groupBoxCameraProperties";
+            this.groupBoxCameraProperties.Size = new System.Drawing.Size(320, 76);
+            this.groupBoxCameraProperties.TabIndex = 12;
+            this.groupBoxCameraProperties.TabStop = false;
+            this.groupBoxCameraProperties.Text = "Camera Properties";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(6, 47);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 21);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Resolution";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(133, 47);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(181, 21);
+            this.comboBox1.TabIndex = 13;
+            // 
+            // groupBoxFileProperties
+            // 
+            this.groupBoxFileProperties.Controls.Add(this.buttonChageDir);
+            this.groupBoxFileProperties.Controls.Add(this.buttonSavePicture);
+            this.groupBoxFileProperties.Controls.Add(this.label2);
+            this.groupBoxFileProperties.Controls.Add(this.comboBoxInterval);
+            this.groupBoxFileProperties.Controls.Add(this.labelFilePrefix);
+            this.groupBoxFileProperties.Controls.Add(this.label1);
+            this.groupBoxFileProperties.Controls.Add(this.labelRecentFilename);
+            this.groupBoxFileProperties.Controls.Add(this.labelDirectory);
+            this.groupBoxFileProperties.Location = new System.Drawing.Point(664, 94);
+            this.groupBoxFileProperties.Name = "groupBoxFileProperties";
+            this.groupBoxFileProperties.Size = new System.Drawing.Size(320, 188);
+            this.groupBoxFileProperties.TabIndex = 13;
+            this.groupBoxFileProperties.TabStop = false;
+            this.groupBoxFileProperties.Text = "File Properties";
+            this.groupBoxFileProperties.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(995, 383);
+            this.Controls.Add(this.groupBoxFileProperties);
+            this.Controls.Add(this.groupBoxCameraProperties);
+            this.Controls.Add(this.pictureBoxVideoDisplay);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "CASISduino Photographer";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxVideoDisplay)).EndInit();
+            this.groupBoxCameraProperties.ResumeLayout(false);
+            this.groupBoxFileProperties.ResumeLayout(false);
+            this.groupBoxFileProperties.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBoxVideoDisplay;
+        private System.Windows.Forms.ComboBox comboBoxCamSelect;
+        private System.Windows.Forms.Button buttonOpenCamera;
+        private System.Windows.Forms.Label labelRecentFilename;
+        private System.Windows.Forms.Button buttonSavePicture;
+        private System.Windows.Forms.Button buttonChageDir;
+        private System.Windows.Forms.FolderBrowserDialog directoryDialog;
+        private System.Windows.Forms.Label labelDirectory;
+        private System.Windows.Forms.TextBox labelFilePrefix;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBoxInterval;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBoxCameraProperties;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBoxFileProperties;
+    }
+}
+
---
+++ CASISduino Photographer/CASISduino Photographer/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//for webcam
+using AForge.Video;
+using AForge.Video.DirectShow;
+//screenshots
+using System.Drawing.Imaging;
+
+
+namespace CASISduino_Photographer
+{
+    public partial class Form1 : Form
+    {
+        //webcam
+        private FilterInfoCollection videoDevices;
+        private VideoCaptureDevice videoSource;
+        //screenshot
+        private static Bitmap bmpScreenshot;
+        private static Graphics gfxScreenshot;
+        //default directory
+        string directory = "";
+        bool flagForSave = false;
+
+        public void print(string str)
+        {
+            System.Diagnostics.Debug.WriteLine(str);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+            foreach (FilterInfo device in videoDevices)
+            {
+                comboBoxCamSelect.Items.Add(device.Name);
+            }
+            comboBoxCamSelect.SelectedIndex = 0;
+            videoSource = new VideoCaptureDevice();
+            labelRecentFilename.Text = "Most Recent Filename: None";
+            buttonSavePicture.Enabled = false;
+        }
+        private void buttonTakePicture_Click(object sender, EventArgs e)
+        {
+            if (videoSource.IsRunning)
+            {
+                videoSource.Stop();
+                pictureBoxVideoDisplay.Image = null;//eliminate image
+                pictureBoxVideoDisplay.Invalidate();
+                buttonSavePicture.Enabled = false;
+            }
+            else
+            {
+                buttonOpenCamera.Text = "Close Camera";
+                buttonSavePicture.Enabled = true;
+                videoSource = new VideoCaptureDevice(videoDevices[comboBoxCamSelect.SelectedIndex].MonikerString);
+                videoSource.VideoResolution = videoSource.VideoCapabilities[12];//set to max resolution
+                //set NewFrame Event Handler
+                videoSource.NewFrame += new NewFrameEventHandler(videoSource_NewFrame);
+                videoSource.Start();
+            }
+            if (directory == "")//if not changed
+            {
+                buttonChageDir.PerformClick();
+            }
+        }
+        void videoSource_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap frame= (Bitmap)eventArgs.Frame.Clone();
+            pictureBoxVideoDisplay.Image = frame;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (videoSource.IsRunning)
+            {
+                videoSource.Stop();
+            }
+        }
+
+        private void pictureBoxVideoDisplay_Click(object sender, EventArgs e)
+        {
+
+        }
+        string createFilename()
+        {
+            string timestampStr = Convert.ToString(DateTime.Now);
+            string timestampStr2 = timestampStr.Replace('/', '-');
+            string timestampStr3 = timestampStr2.Replace(' ', '_');
+            string timestamp = timestampStr3.Replace(':', '-');
+            string filename = labelFilePrefix.Text+ "_" + timestamp + ".jpeg";
+
+            return filename;
+        } 
+        
+        private void buttonSavePicture_Click(object sender, EventArgs e)
+        {
+            string filename = createFilename();
+            labelRecentFilename.Text = "Most Recent Filename: " + filename;
+            pictureBoxVideoDisplay.Image.Save(directory + filename, ImageFormat.Jpeg);
+            /*
+            //take screenshot of the windows form and save it
+            // Set the bitmap object to the size of the screen
+            bmpScreenshot = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height, PixelFormat.Format32bppArgb);
+            // Create a graphics object from the bitmap
+            gfxScreenshot = Graphics.FromImage(bmpScreenshot);
+            // Take the screenshot from the upper left corner to the right bottom corner
+            gfxScreenshot.CopyFromScreen(Screen.PrimaryScreen.Bounds.X, Screen.PrimaryScreen.Bounds.Y, 0, 0, Screen.PrimaryScreen.Bounds.Size, CopyPixelOperation.SourceCopy);
+            // Save the screenshot to the specified path that the user has chosen
+            bmpScreenshot.Save(directory + Convert.ToChar(92) + filename, ImageFormat.Png);
+            */
+        }
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void folderBrowserDialog1_HelpRequest_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void saveScreenshot_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void buttonChageDir_Click(object sender, EventArgs e)
+        {
+            directoryDialog.Description = "Select a folder to which all photos will be saved";
+            directoryDialog.ShowNewFolderButton = true;
+            directoryDialog.ShowDialog();
+            directory = directoryDialog.SelectedPath + Convert.ToChar(92);
+            labelDirectory.Text = directoryDialog.SelectedPath;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelTimestamp_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBoxInterval_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
